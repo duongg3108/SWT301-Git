e41cbaec2158e823ec1e55e4ec8ed8b59e8ea2fd
PhuongVy297 <phuongzyxinh297@gmail.com>
2026-06-22 12:10:41 +0700

    Update Student class with dateOfBirth and GPA

--- QuanLySinhVien/src/Students.java
+++ QuanLySinhVien/src/Students.java
@@ -2,19 +2,22 @@ public class Students {
     // Các thuộc tính của sinh viên
     private String studentId;
     private String fullName;
-    private int age;
+    private String dateOfBirth;
+    private double gpa;
 
     // Hàm khởi tạo (Constructor)
     public Students(String studentId, String fullName, int age) {
         this.studentId = studentId;
         this.fullName = fullName;
-        this.age = age;
+        this.dateOfBirth = dateOfBirth;
+        this.gpa = gpa;
     }
 
     // Phương thức hiển thị thông tin
     public void displayStudentInfo() {
         System.out.println("Mã SV: " + studentId);
         System.out.println("Họ và tên: " + fullName);
-        System.out.println("Tuổi: " + age);
+        System.out.println("Ngày sinh: " + dateOfBirth); 
+        System.out.println("Điểm GPA: " + gpa);
     }
 }
