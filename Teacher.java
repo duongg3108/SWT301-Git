f87c7ab734cc73a6bb6502121685deed4b876239
longg21082005-dot <longg21082005@gmail.com>
2026-06-22 12:02:45 +0700

    Test teacher

--- Teacher.java
+++ Teacher.java
@@ -1,6 +1,8 @@
-public class TeacherFPT {
-    public static void main(String[] args) {
+public class Teacher {
+    public class TeacherFPT {
+        public static void main(String[] args) {
 
-        System.out.println("Teacher");
+            System.out.println("Teacher");
+        }
     }
 }
